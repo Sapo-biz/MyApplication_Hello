f1dbeec035e7fb160c86895aca64748d01ba6bf1
Jason He <sapo.business.ventures@gmail.com>
2025-11-08 22:13:32 -0800

    fragment

---
+++ app/src/main/java/com/example/myapplication_hello/ButtonFragment.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication_hello
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class ButtonFragment : Fragment(R.layout.fragment_button) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Additional setup can be added here if needed in the future
+    }
+}
+
